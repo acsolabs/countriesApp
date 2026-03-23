b95dc6bc593f0eb8417f86b6b0f3303aa5977d3b
acsolabs <sofian.achbabi@gmail.com>
2026-03-23 08:40:08 +0100

    fix: implement case-insensitive country name filtering

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
-// https://restcountries.com/v3.1/all?fields=name ------------------------------------
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.------------------------------------------------------------------------------
-// 3 - Passer les données à une variable----------------------------------------------
-
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-
 // Feature : ajouter les currencies
 // --------------------------------------------------------------------------------------
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
@@ -26,6 +19,8 @@ let countries = [];
 // -------------
 // functions
 // -------------
+
+//FetchCountries fetch countries data from API
 const fetchCountries = async () => {
   await fetch(
     "https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,language,region,code,demonym,population,translations",
@@ -33,33 +28,38 @@ const fetchCountries = async () => {
   countriesDisplay();
 };
 
+//countriesDisplay display card to the DOM
 const countriesDisplay = () => {
   // countries.length = 12; //display limit during dev
   countriesContainer.innerHTML = countries
-    .filter((country) => country.name.official.includes(inputSearch.value))
+    .filter((country) =>
+      country.name.official
+        .toLowerCase()
+        .includes(inputSearch.value.toLowerCase()),
+    )
     .map((country) => {
-      return ` <div class="card">
-    <div class="tilt">
-    <div class="img">
-    <img src="${country.flags.png}" alt="${country.flags.alt}">
-    </div>
-    </div>
-    <div class="info">
-    <div class="cat">${country.region}</div>
-    <h2 class="title">${country.name.official}</h2>
-    <p class="desc">Capital City: ${country?.capital ?? "no information"}</p>
-    <div class="feats">
-    <span class="feat">${country.population} hab</span>
-    <span class="feat">devise</span>
-    </div>
-    <div class="bottom">
-    <button class="btn">
-    <span>Read more</span>
-    </button>
-    </div>
-    </div>
-    </div>
-    `;
+      return ` 
+              < class="card">
+                <div class="tilt">
+                  <div class="img">
+                  <img src="${country.flags.png}" alt="${country.flags.alt}">
+                  </div>
+                </div>
+                <div class="info">
+                  <div class="cat">${country.region}</div>
+                  <h2 class="title">${country.name.official}</h2>
+                  <p class="desc">Capital City: ${country?.capital ?? "no information"}</p>
+                  <div class="feats">
+                  <span class="feat">${country.population} hab</span>
+                  <span class="feat">devise</span>
+                </div>
+                <div class="bottom">
+                  <button class="btn">
+                    <span>Read more</span>
+                  </button>
+                </div>
+              </div>
+            `;
     })
     .join("");
 };
